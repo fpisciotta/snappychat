f2de21070538dad3af595143774d1b89f4ad5d97
Fabrizio Pisciotta <fabriziojps@gmail.com>
2016-11-30 16:43:51 -0800

    chat resources added

--- models/model_chat.js
+++ models/model_chat.js
@@ -7,45 +7,54 @@ var Chat = chatModel.Chat
 var ChatMessage = chatModel.ChatMessage;
 
 var createChat = function (query, callback) {
-
-	var new_chat = {
-		user_creator_id: query.user_sender_id,
-		user_receiver_id: query.user_receiver_id
-	}
-	var chat = new Chat(new_chat)
+	var chat = new Chat(query)
 	chat.save(function (err) {
-		console.log("Chat "+JSON.stringify(chat));
 		callback(err, chat);
 	});
 }
 
 exports.deleteChat = function (query, callback) {
-	User.find({
-		'email': {$in : [query.user_creator_id,query.user_receiver_id]}
-	}).exec(function (err, users) {
-
+	User.findOne({email:query.user_creator_id}).exec(function (err, user_creator) {
+		//console.log("User sender: "+JSON.stringify(user_sender));
 		if (err)
 			return callback(err, null);
 
-		if (users == undefined || users == null || !users.length)
-			return callback(new Error("Users sender not found"), null);
+		if (user_creator == undefined || user_creator == null)
+			return callback(new Error("User sender not found"), null);
 
-		if (users.length < 2)
-			return callback(new Error("Users not found"), null);
+		User.findOne({email:query.user_receiver_id}).exec(function (err, user_receiver) {
+			//console.log("User receiver: "+JSON.stringify(user_receiver));
+			if (err)
+				return callback(err, null);
+
+			if (user_receiver == undefined || user_receiver == null)
+				return callback(new Error("User receiver not found"), null);
 		
-		Chat.find({
-			'user_creator_id' : {$in : [users[0]._id,users[1]._id]},
-			'user_receiver_id' : {$in : [users[0]._id,users[1]._id]},
-		}).exec(function(err,chat){
-			ChatMessage.remove({chat_id : chat._id},function(err){
-				if(err)
-					callback(err,null);
-				console.log("Chat: "+JSON.stringify(chat));
-				Chat.remove(chat,callback);
-			})
+			var users = [user_creator,user_receiver];
+			
+			Chat.findOne({
+				'user_creator_id' : users[0]._id,
+				'user_receiver_id' : users[1]._id
+			}).exec(function(err,chat){
+				
+				if (err)
+					return callback(err, null);
+
+				if (chat == undefined || chat == null)
+					return callback(new Error("Chat not found"), null);
+				
+				ChatMessage.remove({chat_id : chat._id},function(err){
+					if(err)
+						callback(err,null);
+					console.log("Chat: "+JSON.stringify(chat));
+					chat.remove(chat,callback);
+				})
+			});
+		
 		});
 	
 	});
+		
 }
 
 var updateChat = function (query, callback) {
@@ -84,6 +93,84 @@ var updateChat = function (query, callback) {
 }
 
 exports.createChatMessage = function (query, callback) {
+	var users;
+	User.findOne({email:query.user_sender_id}).exec(function (err, user_sender) {
+		//console.log("User sender: "+JSON.stringify(user_sender));
+		if (err)
+			return callback(err, null);
+
+		if (user_sender == undefined || user_sender == null)
+			return callback(new Error("User sender not found"), null);
+
+		User.findOne({email:query.user_receiver_id}).exec(function (err, user_receiver) {
+			//console.log("User receiver: "+JSON.stringify(user_receiver));
+			if (err)
+				return callback(err, null);
+
+			if (user_receiver == undefined || user_receiver == null)
+				return callback(new Error("User receiver not found"), null);
+		
+			users = [user_sender,user_receiver];
+			
+			//Search for chat conversations
+			Chat.find({
+				'user_creator_id' : {$in : [users[0]._id,users[1]._id]},
+				'user_receiver_id' : {$in : [users[0]._id,users[1]._id]}
+			}).exec(function (err, chat) {
+				if (err)
+					return callback(err, null);
+
+				if (chat == undefined || chat == null || !chat.length) {
+					var new_chat = {
+						user_creator_id: users[0]._id,
+						user_receiver_id: users[1]._id
+					}
+					createChat(new_chat, function (err, chat) {
+						if (err)
+							return callback(err, null);
+						if (chat == undefined || chat == null)
+							return callback(new Error("Chat not found"), null);
+						
+						saveChatMessage(chat, users, query.message, callback);
+					});
+				} else {
+					console.log("Chat found");
+					saveChatMessage(chat, [user_sender,user_receiver], query.message, callback);
+				}
+
+			});
+			
+			
+		});
+
+		
+	});
+}
+
+var saveChatMessage = function (chat, users, message, callback) {
+
+	var new_chat_message = {
+		chat_id: chat._id,
+		user_sender_id: users[0]._id,
+		user_receiver_id: users[1]._id,
+		message: message
+	}
+	var chatMessage = new ChatMessage(new_chat_message);
+	//Store Chat_message
+	chatMessage.save(function (err) {
+		if (err)
+			return callback(err, null);
+		console.log("Chat  "+JSON.stringify(chat));
+		console.log("Chat message "+JSON.stringify(chatMessage));
+		//chat.chat_messages.push(chatMessage);
+		//chat.save(callback);
+		Chat.update(chat,{$push: {"chat_messages": chatMessage._id}},callback)
+
+	});
+
+}
+
+exports.getChatHistory = function (query, callback) {
 	User.find({
 		'email': {$in : [query.user_sender_id,query.user_receiver_id]}
 	}).exec(function (err, users) {
@@ -96,95 +183,11 @@ exports.createChatMessage = function (query, callback) {
 
 		if (users.length < 2)
 			return callback(new Error("Users not found"), null);
-
-		var chat1 = {
-			user_creator_id: users[0]._id,
-			user_receiver_id: users[1]._id
-		}
-
-		var chat2 = {
-			user_creator_id: users[1]._id,
-			user_receiver_id: users[0]._id
-		}
+		
 		//Search for chat conversations
 		Chat.find({
-			'user_creator_id' : {$in : [users[0]._id,users[1]._id]},
-			'user_receiver_id' : {$in : [users[0]._id,users[1]._id]},
-		}).exec(function (err, chat) {
-			if (err)
-				return callback(err, null);
-
-			if (chat == undefined || chat == null || !chat.length) {
-				//Create chat conversation if it doesn't exist
-				createChat({
-					user_sender_id: users[0]._id,
-					user_receiver_id: users[1]._id
-				}, function (err, chat) {
-					if (err)
-						return callback(err, null);
-					if (chat == undefined || chat == null)
-						return callback(new Error("Chat not found"), null);
-					
-					saveChatMessage(chat, users, query.message, callback);
-				});
-			} else {
-				console.log("Chat found");
-				saveChatMessage(chat, users, query.message, callback);
-			}
-
-		});
-	});
-}
-
-var saveChatMessage = function (chat, users, message, callback) {
-
-	var new_chat_message = {
-		chat_id: chat._id,
-		user_sender_id: users[0]._id,
-		user_receiver_id: users[1]._id,
-		message: message,
-	}
-	var chatMessage = new ChatMessage(new_chat_message);
-	//Store Chat_message
-	chatMessage.save(function (err) {
-		if (err)
-			return callback(err, null);
-		console.log("Chat  "+JSON.stringify(chat));
-		console.log("Chat message "+JSON.stringify(chatMessage));
-		chat.chat_messages.push(chatMessage);
-		chat.save(callback);
-		//Chat.update(chat,{$push: {"chat_message": chatMessage._id}},callback)
-
-	});
-
-}
-
-exports.getChatHistory = function (query, callback) {
-	User.find({
-		'email': {$in : [query.user_sender_id,query.user_receiver_id]}
-	}).exec(function (err, users) {
-		if (err)
-			return callback(err, null);
-
-		if (users == undefined || users == null)
-			return callback(new Error("Users sender not found"), null);
-
-		if (users.length < 2)
-			return callback(new Error("Users not found"), null);
-
-		var chat1 = {
-			user_creator_id: users[0]._id,
-			user_receiver_id: users[1]._id
-		}
-
-		var chat2 = {
-			user_creator_id: users[1]._id,
-			user_receiver_id: users[0]._id
-		}
-		//Search for chat conversations
-		Chat.find({
-			'user_creator_id' : {$in : [users[0]._id,users[1]._id]},
-			'user_receiver_id' : {$in : [users[0]._id,users[1]._id]},
+			'user_creator_id' : users[0]._id,
+			'user_receiver_id' : users[1]._id
 		}).populate([{path:'user_creator_id', select:'name email nick_name'},
 		{path:'user_receiver_id', select:'name email nick_name'},
 		{path:'chat_messages', populate: [{
@@ -195,6 +198,7 @@ exports.getChatHistory = function (query, callback) {
 			select:'email'
 		}]}])
 		.exec(function (err, chat) {
+			console.log("Chat: "+JSON.stringify(chat));
 			if (err)
 				return callback(err, null);
 
